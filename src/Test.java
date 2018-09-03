306dcfb55fd2921306c9af1bbdbaf8141bd808fb
cice <cice@A8POVMac11.local>
2018-09-03 21:52:53 +0200

    mensaje2

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,16 @@
+import hilos.HiloPropio;
+
 public class Test {
 
     public static void main(String[] args) {
 
 
+        HiloPropio hp = new HiloPropio();
+        hp.start();
 
+        for (int i=0;i<100;i++){
+            System.out.println("Hilo KING: " + i);
 
-
-
+        }
     }
 }
--- src/hilos/HiloPropio.java
+++ src/hilos/HiloPropio.java
@@ -1,4 +1,12 @@
 package hilos;
 
 public class HiloPropio extends Thread {
+
+
+    public void run(){
+        for (int i=0;i<100;i++){
+            System.out.println("Hilo ANACLETUS: " + i);
+
+        }
+    }
 }
